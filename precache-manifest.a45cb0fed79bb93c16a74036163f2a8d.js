f2d28d4985ad74f9a46a2c318d45f771c4ef0b44
Povilas Urbonas <povilas.urbonas@canaldigital.com>
2019-12-03 09:02:47 +0100

    Updates

---
+++ precache-manifest.a45cb0fed79bb93c16a74036163f2a8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "777e69efa90e9961d9bb552d9f07aae7",
+    "url": "/povilas-app/index.html"
+  },
+  {
+    "revision": "f9ae70e21abb7f47d2c5",
+    "url": "/povilas-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4a2c944a5801d6ae557c",
+    "url": "/povilas-app/static/js/2.c21e2b2e.chunk.js"
+  },
+  {
+    "revision": "f9ae70e21abb7f47d2c5",
+    "url": "/povilas-app/static/js/main.0d524386.chunk.js"
+  },
+  {
+    "revision": "7c6ab70b8b73936b140b",
+    "url": "/povilas-app/static/js/runtime-main.fa4a83d6.js"
+  }
+]);
